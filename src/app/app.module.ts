5a3856779e2577c32248a40be2606c2ebf7c6a64
v1itoof <joaov1c@outlook.com>
2023-07-08 17:19:14 -0300

    Alterações Cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    IonicModule.forRoot(),
     NgbDropdownModule,
     AppRoutingModule
   ],
